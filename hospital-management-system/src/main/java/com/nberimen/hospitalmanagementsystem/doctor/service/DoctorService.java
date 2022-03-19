9569394f0908ae1b18078d0c4932b7d7216b799a
Necati Berimen <berimen66@gmail.com>
2022-03-19 18:55:46 +0300

    Create DoctorService

---
+++ hospital-management-system/src/main/java/com/nberimen/hospitalmanagementsystem/doctor/service/DoctorService.java
@@ -0,0 +1,42 @@
+package com.nberimen.hospitalmanagementsystem.doctor.service;
+
+import com.nberimen.hospitalmanagementsystem.doctor.converter.DoctorMapper;
+import com.nberimen.hospitalmanagementsystem.doctor.dto.DoctorDto;
+import com.nberimen.hospitalmanagementsystem.doctor.dto.DoctorSaveRequestDto;
+import com.nberimen.hospitalmanagementsystem.doctor.entity.Doctor;
+import com.nberimen.hospitalmanagementsystem.doctor.service.entityservice.DoctorEntityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DoctorService {
+
+    private final DoctorEntityService doctorEntityService;
+
+    public List<DoctorDto> findAll() {
+        List<Doctor> doctorList = doctorEntityService.findAll();
+        List<DoctorDto> doctorDtoList = DoctorMapper.INSTANCE.convertToDoctorDtoList(doctorList);
+        return doctorDtoList;
+    }
+
+    public DoctorDto save(DoctorSaveRequestDto doctorSaveRequestDto) {
+        Doctor doctor = DoctorMapper.INSTANCE.convertToDoctor(doctorSaveRequestDto);
+        doctor = doctorEntityService.save(doctor);
+        DoctorDto doctorDto = DoctorMapper.INSTANCE.convertToDoctorDto(doctor);
+        return doctorDto;
+    }
+
+    public void delete(Long id) {
+        Doctor inDB = doctorEntityService.getById(id);
+        doctorEntityService.delete(inDB);
+    }
+
+    public DoctorDto findById(Long id) {
+        Doctor inDB = doctorEntityService.getById(id);
+        DoctorDto doctorDto = DoctorMapper.INSTANCE.convertToDoctorDto(inDB);
+        return doctorDto;
+    }
+}
