cd03b314e79b496bf7eab998c309c76960a79510
Necati Berimen <berimen66@gmail.com>
2022-03-19 18:53:54 +0300

    Create DoctorMapper

---
+++ hospital-management-system/src/main/java/com/nberimen/hospitalmanagementsystem/doctor/converter/DoctorMapper.java
@@ -0,0 +1,23 @@
+package com.nberimen.hospitalmanagementsystem.doctor.converter;
+
+import com.nberimen.hospitalmanagementsystem.doctor.dto.DoctorDto;
+import com.nberimen.hospitalmanagementsystem.doctor.dto.DoctorSaveRequestDto;
+import com.nberimen.hospitalmanagementsystem.doctor.entity.Doctor;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface DoctorMapper {
+
+    DoctorMapper INSTANCE = Mappers.getMapper(DoctorMapper.class);
+
+    List<DoctorDto> convertToDoctorDtoList(List<Doctor> doctorList);
+
+    Doctor convertToDoctor(DoctorSaveRequestDto doctorSaveRequestDto);
+
+    DoctorDto convertToDoctorDto(Doctor doctor);
+
+}
