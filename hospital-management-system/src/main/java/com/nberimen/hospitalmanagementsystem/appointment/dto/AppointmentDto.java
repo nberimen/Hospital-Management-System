a9dfa7173f2bba3fc5c11e2b6d18af74afcf73de
Necati Berimen <berimen66@gmail.com>
2022-03-19 15:00:39 +0300

    Update AppointmentDto & AppointmentSaveRequestDto

--- hospital-management-system/src/main/java/com/nberimen/hospitalmanagementsystem/appointment/dto/AppointmentDto.java
+++ hospital-management-system/src/main/java/com/nberimen/hospitalmanagementsystem/appointment/dto/AppointmentDto.java
@@ -2,6 +2,13 @@ package com.nberimen.hospitalmanagementsystem.appointment.dto;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class AppointmentDto {
+
+    private Long id;
+    private Long patientId;
+    private Long doctorId;
+    private Date appointmentDate;
 }
--- hospital-management-system/src/main/java/com/nberimen/hospitalmanagementsystem/appointment/dto/AppointmentSaveRequestDto.java
+++ hospital-management-system/src/main/java/com/nberimen/hospitalmanagementsystem/appointment/dto/AppointmentSaveRequestDto.java
@@ -2,6 +2,12 @@ package com.nberimen.hospitalmanagementsystem.appointment.dto;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class AppointmentSaveRequestDto {
+
+    private Long patientId;
+    private Long doctorId;
+    private Date appointmentDate;
 }
