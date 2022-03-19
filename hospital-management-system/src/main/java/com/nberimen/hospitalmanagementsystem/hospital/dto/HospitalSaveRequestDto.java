e388b186b61e9f395984077c04ff4f51fdf974e3
Necati Berimen <berimen66@gmail.com>
2022-03-19 13:47:29 +0300

    Create hospital module

--- hospital-management-system/src/main/java/com/nberimen/hospitalmanagementsystem/hospital/dto/HospitalSaveRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.nberimen.hospitalmanagementsystem.hospital.dto;
-
-import lombok.Data;
-
-@Data
-public class HospitalSaveRequestDto {
-    private Long cityId;
-    private String name;
-}
