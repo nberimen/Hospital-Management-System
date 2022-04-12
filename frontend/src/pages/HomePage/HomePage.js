4a6f28b4f96c8814c3f2f79153c922b6d210768d
Necati Berimen <berimen66@gmail.com>
2022-04-13 00:04:01 +0300

    create HomePage

---
+++ frontend/src/pages/HomePage/HomePage.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { FaUserCheck } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import png from "../../assets/17-825x510.jpg";
+
+const HomePage = () => {
+  return (
+    <>
+      <section className="mt-5">
+        <div className="container">
+          <div className="pt-5">
+            <div className="row">
+              <div className="col-lg-5 p-5">
+                <h1 className="fw-bold" style={{ marginTop: "5rem" }}>
+                  Hemen Randevu Al
+                </h1>
+              </div>
+
+              <div
+                className="col-lg-6 bg-white"
+                style={{
+                  borderTopLeftRadius: "5rem",
+                  borderBottomRightRadius: "5rem",
+                }}
+              >
+                <img
+                  className="mt-2 d-md-none d-none d-lg-block"
+                  src={png}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="apply-steps">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <div>
+                <FaUserCheck className="large-icon" />
+                <Link className="nav-link" to="/patient">
+                  <span className="ms-3"> Hemen Kayıt Ol</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HomePage;
