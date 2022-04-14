168a28327e3f9d724691650d67179cff3fbf4837
Necati Berimen <berimen66@gmail.com>
2022-04-15 01:17:49 +0300

    create AdminPage.js

---
+++ frontend/src/pages/AdminPage/AdminPage.js
@@ -0,0 +1,38 @@
+import React from "react";
+import png from "../../assets/1-12924.jpg";
+
+const AdminPage = () => {
+  return (
+    <>
+      <section className="mt-5">
+        <div className="container">
+          <div className="pt-5">
+            <div className="row">
+              <div className="col-lg-5 p-5">
+                <h1 className="display-1">Hello Admin</h1>
+                <h5 className="display-6">
+                  Welcome to Hospital Management System.
+                </h5>
+              </div>
+
+              <div
+                className="col-lg-6 bg-white"
+                style={{
+                  borderTopLeftRadius: "5rem",
+                  borderBottomRightRadius: "5rem",
+                }}
+              >
+                <img
+                  className="mt-2 d-md-none d-none d-lg-block"
+                  src={png}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default AdminPage;
