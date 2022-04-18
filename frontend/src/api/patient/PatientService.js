4b4353ff5c34956649104aaf9bfa7da079b86cd6
Necati Berimen <berimen66@gmail.com>
2022-04-19 00:12:24 +0300

    get all appointments && get all active appointments && create appointment

--- frontend/src/api/patient/PatientService.js
+++ frontend/src/api/patient/PatientService.js
@@ -1,17 +1,27 @@
 import axios from "axios";
 
 export const getAll = () => {
-    return axios.get(`/api/v1/patients`);
-  };
-  
-  export const save = (creds) => {
-    return axios.post(`/api/v1/patients`, creds);
+  return axios.get(`/api/v1/patients`);
 };
-  
+
+export const save = (creds) => {
+  return axios.post(`/api/v1/patients`, creds);
+};
+
 export const getById = (id) => {
-    return axios.get(`/api/v1/patients/${id}`);
+  return axios.get(`/api/v1/patients/${id}`);
 };
 
 export const remove = (id) => {
-    return axios.delete(`/api/v1/patients/${id}`);
+  return axios.delete(`/api/v1/patients/${id}`);
+};
+
+export const createAppointment = (body) => {
+  return axios.post(`/api/v1/appointments`, body);
+};
+export const getAllByDoctorIdAndPatientId = (patientId, doctorId) => {
+  return axios.get(`/api/v1/appointments/active/${patientId}/${doctorId}`);
+};
+export const getAllByPatientId = (patientId) => {
+  return axios.get(`/api/v1/appointments/patient/${patientId}`);
 };
